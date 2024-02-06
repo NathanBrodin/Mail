7359444d3b8d5cec3850997da7bce3532397f47e
Nathan Brodin <nathan@brodin.dev>
2024-02-06 22:23:00 +0100

    test: fixed the fix that was supposed to fix the other fix

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -10,12 +10,4 @@ describe('User can acces login', () => {
     cy.get('[data-cy="get-started-button"]').click();
     cy.url().should('include', '/auth/signin');
   });
-
-  it('Unauthenticated user should be able to go to the login page via sign in button on large screens', () => {
-    cy.viewport(1024, 768);
-    cy.visit('/discover');
-
-    cy.get('[data-cy="sign-in-button"]').click();
-    cy.url().should('include', '/auth/signin');
-  });
 });
