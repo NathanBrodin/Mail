574d23f7b96c93e9aa0b4f6b84f5e88e6a290406
Nathan Brodin <nathan@brodin.dev>
2024-02-07 09:19:17 +0100

    fix: uncaught SyntaxError: expected expression, got '<'

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -4,10 +4,27 @@ describe('User can acces login', () => {
     cy.url().should('include', '/discover');
   });
 
-  it('Unauthenticated user should be able to go to the login page via get started button', () => {
+  it.only('Unauthenticated user should be able to go to the login page via get started button', () => {
     cy.visit('/');
 
     cy.get('[data-cy="get-started-button"]').click();
+    cy.url().should('include', '/auth/signin');
+  });
+
+  it('Unauthenticated user should be able to go to the login page via sign in button on large screens', () => {
+    cy.viewport(1024, 768);
+    cy.visit('/discover');
+
+    cy.get('[data-cy="sign-in-button"]').click();
+    cy.url().should('include', '/auth/signin');
+  });
+
+  it('Unauthenticated user should be able to go to the login page via sign in button on small screens', () => {
+    cy.viewport('iphone-8');
+    cy.visit('/discover');
+
+    cy.get('[data-cy="open-menu-button"]').click();
+    cy.get('[data-cy="sign-in-button-mobile"]').click();
     cy.url().should('include', '/auth/signin');
   });
 });
