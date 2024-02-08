07712c06ac729ded3c56fca3eedc09f7295f5702
Nathan Brodin <nathan@brodin.dev>
2024-02-08 09:54:50 +0100

    build: commit before making chaos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,11 +93,74 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'fade-in': {
+          '0%': {
+            opacity: '0%',
+          },
+          '75%': {
+            opacity: '0%',
+          },
+          '100%': {
+            opacity: '100%',
+          },
+        },
+        'fade-left': {
+          '0%': {
+            transform: 'translateX(100%)',
+            opacity: '0%',
+          },
+
+          '30%': {
+            transform: 'translateX(0%)',
+            opacity: '100%',
+          },
+          '100%': {
+            opacity: '0%',
+          },
+        },
+        'fade-right': {
+          '0%': {
+            transform: 'translateX(-100%)',
+            opacity: '0%',
+          },
+
+          '30%': {
+            transform: 'translateX(0%)',
+            opacity: '100%',
+          },
+          '100%': {
+            opacity: '0%',
+          },
+        },
+        title: {
+          '0%': {
+            'line-height': '0%',
+            'letter-spacing': '0.25em',
+            opacity: '0',
+          },
+          '25%': {
+            'line-height': '0%',
+            opacity: '0%',
+          },
+          '80%': {
+            opacity: '100%',
+          },
+
+          '100%': {
+            'line-height': '100%',
+            opacity: '100%',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'spin-slow': 'spin 6s linear infinite',
+        'spin-slow': 'spin 12s ease-in-out infinite',
+        'pulse-slow': 'pulse 4s linear infinite',
+        'fade-in': 'fade-in 3s ease-in-out forwards',
+        title: 'title 3s ease-out forwards',
+        'fade-left': 'fade-left 3s ease-in-out forwards',
+        'fade-right': 'fade-right 3s ease-in-out forwards',
       },
     },
   },
